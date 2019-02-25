ff20b96364d62d03b931ff6bd5fed9c954e603b0
peterlemon <krom@gbadev.org>
2019-02-25 17:37:20 +0000

    Optimize Fill Screen Demo using ldir memory copy instruction

--- Screen/FillScreen/FillScreen.asm
+++ Screen/FillScreen/FillScreen.asm
@@ -13,15 +13,10 @@ seek($5CCB) ; Start: // Entry Point Of Code
 include "LIB/ZXSPECTRUM.INC" // Include ZX Spectrum Definitions
 
 // Fill Screen Bitmap & Color Area With Screen Data
-ld de,Screen+Screen.size-1 // DE = Screen Data End Address
-ld hl,SCR_COL+767 // HL = Screen Color Area End Address ($5800+767)
-FillSCR:
-  ld a,(de)     // A = Screen Data Byte
-  dec de        // Decrement Screen Data Address (HL--)
-  ld (hl),a     // Store Screen Data (A) To Screen Address (HL)
-  dec hl        // Decrement Screen Address (HL--)
-  bit 6,h       // Test Bit 6 Of Screen Address MSB (H)
-  jr nz,FillSCR // IF (Bit 6 Of Screen Address MSB != 0) Fill Screen
+ld bc,Screen.size // BE = Screen Data Size Count (6912 Bytes)
+ld de,SCR_BMP     // DE = Screen Bitmap Area Start Address ($4000)
+ld hl,Screen      // HL = Screen Data Start Address
+ldir // Copy Screen Data To Screen Bitmap/Color Area (WHILE BC > 0 (LD (DE),(HL), DE++, HL++, BC--))
 
 Loop:
   jr Loop
